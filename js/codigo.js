fd5f167169777203583ef06b3bd051f837be7563
ormarrero <oscarrene.marrero.next.contractor@bbva.com>
2023-04-04 18:39:25 +0200

    creando primer commit desde el visual studio code

---
+++ js/codigo.js
@@ -0,0 +1,23 @@
+/*
+            ==============
+             LAS VARIABLES
+            ==============
+    1º Crear Variables
+        a.Sintaxis
+        -> var minuscula="";
+        -> var minuscula=5
+        Genera de manera automatica define el nombre de la variable 
+        y su valor
+        b.Explicitas
+        var antonio;
+        antonio="";
+        c.Implicitas
+        var antonio="";
+*/
+var nombre="Toño";
+swal({
+    title: "Me llamo: " + nombre,
+    text: "NOMBRE DE PILA",
+    button: "Un placer teneros aquí",
+    icon: "success",
+});
